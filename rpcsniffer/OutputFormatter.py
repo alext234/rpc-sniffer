f6e128d9a657e4c3fb165d97ca1fae1b33b399e1
alex <alex.tran234@gmail.com>
2018-03-25 19:25:22 +1100

    add tests for OutputFormatter

--- rpcsniffer/OutputFormatter.py
+++ rpcsniffer/OutputFormatter.py
@@ -12,4 +12,15 @@ class OutputFormatter:
 			"result".ljust(Config.RESULT_SIZE) 
 
 	def __call__(self, jsonObject):
+		if 'method' in jsonObject:
+			return \
+				str(jsonObject['id']).ljust(Config.ID_SIZE) + \
+				jsonObject['method'].ljust(Config.METHOD_SIZE) + \
+				str(jsonObject['params']).ljust(Config.PARAMS_SIZE)
+		elif 'result' in jsonObject:
+			return str(jsonObject['id']).ljust(Config.ID_SIZE) + \
+			' '.ljust(Config.METHOD_SIZE) + \
+			' '.ljust(Config.PARAMS_SIZE) + \
+			jsonObject['result'].ljust(Config.RESULT_SIZE)
+					
 		return ''
--- tests/OutputFormatter_test.py
+++ tests/OutputFormatter_test.py
@@ -20,10 +20,29 @@ class PackerDecoderTestCase(unittest.TestCase):
 		self.assertEqual(columnTitles, expectedColumnTitles)
 		
 
-	def test_OutputFormatterJsonObject(self):
+	def test_OutputFormatterMethodCall(self):
 		jsonObject = json.loads(\
-			'{"id": 1, "params": [], "method": "web3_clientVersion", "jsonrpc": "2.0"}');
+			'{"id": 1, "params": [], "method": "web3_clientVersion", "jsonrpc": "2.0"}')
 		formattedOutput = self.outputFormatter(jsonObject)
-		expectedOutput = "" # TODO to set an expected output here
-		
+		expectedOutput = str(jsonObject['id']).ljust(Config.ID_SIZE) + \
+			jsonObject['method'].ljust(Config.METHOD_SIZE) + \
+			str(jsonObject['params']).ljust(Config.PARAMS_SIZE)
+					
 		self.assertEqual(formattedOutput, expectedOutput)
+		
+
+	def test_OutputFormatterResponse(self):
+		jsonObject = json.loads(\
+		'{"jsonrpc":"2.0","id":1,"result":"Geth/v1.8.2-stable/linux-amd64/go1.9.2"}')
+		formattedOutput = self.outputFormatter(jsonObject)
+		expectedOutput = str(jsonObject['id']).ljust(Config.ID_SIZE) + \
+			' '.ljust(Config.METHOD_SIZE) + \
+			' '.ljust(Config.PARAMS_SIZE) + \
+			jsonObject['result'].ljust(Config.RESULT_SIZE)
+			
+					
+		self.assertEqual(formattedOutput, expectedOutput)
+		
+		
+		
+		
