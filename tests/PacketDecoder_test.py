d58e9807b4272c8c5d6602c9955da01a2ec47a94
alex <alex.tran234@gmail.com>
2018-03-25 13:51:44 +1100

    add test for request and response

--- tests/PacketDecoder_test.py
+++ tests/PacketDecoder_test.py
@@ -35,15 +35,21 @@ class PackerDecoderTestCase(unittest.TestCase):
 		PacketDecoder should decode correctly the json fields of a method call request
 		"""
 		packets = rdpcap(self.dataDir + '/web3_clientVersion_method_call.pcap')
-		# TODO
-		pass
-
+		packet = packets[0] # there is only 1 packet in the file
+		data = self.packetDecoder(packet)
+		
+		self.assertNotEqual(data, None)
+		self.assertEqual(data['method'], "web3_clientVersion")
+		self.assertEqual(data['params'], [])
 
 	def test_methodCallResults(self):
 		"""
 		PacketDecoder should decode correctly the json fields of a method call response
 		"""
 		packets = rdpcap(self.dataDir + '/web3_clientVersion_result.pcap')
-		# TODO
-		pass
+		packet = packets[0] # there is only 1 packet in the file
+		data = self.packetDecoder(packet)
+		
+		self.assertNotEqual(data, None)
+		self.assertNotEqual(data['result'], "")
 		
